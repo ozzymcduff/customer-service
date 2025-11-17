cce3bd24fc25269f19b7d15e9170cc3242d4b36b
Oskar Mathieu Gewalli <oskar@gewalli.se>
2025-11-17 19:42:47 +0200

    Upgrade to Rails 8

--- rails/app/helpers/customer_service_helper.rb
+++ rails/app/helpers/customer_service_helper.rb
@@ -1,21 +1,21 @@
 module CustomerServiceHelper
   def element(tag, value)
-    if (value == nil)
+    if value == nil
       return "<#{tag} xsi:nil=\"true\"/>".html_safe
     end
-    return "<#{tag}>#{value.to_s.encode(:xml => :text)}</#{tag}>".html_safe
+    "<#{tag}>#{value.to_s.encode(xml: :text)}</#{tag}>".html_safe
   end
-  
+
   def self.xml_hash_to_ruby_convention(hash)
-    return Hash[ hash.map { |key, value| 
-      [underscore(key), value_or_nil(value)]
+    Hash[ hash.map { |key, value|
+      [ underscore(key), value_or_nil(value) ]
     }]
   end
   private
   def self.underscore(name)
-    name.gsub(/::/, '/').
-    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    name.gsub(/::/, "/").
+    gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+    gsub(/([a-z\d])([A-Z])/, '\1_\2').
     tr("-", "_").
     downcase
   end
@@ -23,6 +23,6 @@ module CustomerServiceHelper
     if value.is_a?(Hash) && value["xsi:nil"] == "true"
       return nil
     end
-    return value
+    value
   end
 end
--- rails/test/controllers/customer_service_controller_test.rb
+++ rails/test/controllers/customer_service_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 $get_all_customers = <<-END
 <?xml version="1.0" encoding="utf-8"?>
@@ -7,13 +7,23 @@ $get_all_customers = <<-END
     xmlns:xsd="http://www.w3.org/2001/XMLSchema"
     xmlns="http://schemas.datacontract.org/2004/07/Customers">
 <Customer>
-  <AccountNumber>1</AccountNumber>
-  <AddressCity xsi:nil="true"/>
-  <AddressCountry xsi:nil="true"/>
-  <AddressStreet xsi:nil="true"/>
-  <FirstName>Oskar</FirstName>
+  <AccountNumber>2</AccountNumber>
+  <AddressCity>Malmoe</AddressCity>
+  <AddressCountry>Sweden</AddressCountry>
+  <AddressStreet>Lugna gatan 40</AddressStreet>
+  <FirstName>Karl</FirstName>
   <Gender>Male</Gender>
-  <LastName>Gewalli</LastName>
+  <LastName>Nilsson</LastName>
+  <PictureUri xsi:nil="true"/>
+</Customer>
+<Customer>
+  <AccountNumber>1</AccountNumber>
+  <AddressCity>Malmoe</AddressCity>
+  <AddressCountry>Sweden</AddressCountry>
+  <AddressStreet>Lugna gatan 40</AddressStreet>
+  <FirstName>Erik</FirstName>
+  <Gender>Male</Gender>
+  <LastName>Andersson</LastName>
   <PictureUri xsi:nil="true"/>
 </Customer>
 </ArrayOfCustomer>
@@ -35,33 +45,33 @@ END
 
 class CustomerServiceControllerTest < ActionDispatch::IntegrationTest
   test "should get all customer" do
-    get "/CustomerService.svc/GetAllCustomers", params: { :format => "xml" }
+    get "/CustomerService.svc/GetAllCustomers", params: { "CONTENT_TYPE" => "application/xml", format: "xml" }
     assert_response :success
-    assert_equal( $get_all_customers, response.body )
+    assert_equal($get_all_customers.strip!, response.body.strip!)
   end
 
   test "should be able to save customer" do
     customer = {
-      "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance", 
-      "xmlns:xsd"=>"http://www.w3.org/2001/XMLSchema", 
-      "xmlns"=>"http://schemas.datacontract.org/2004/07/Customers", 
-      "AccountNumber"=>"1", 
-      "AddressCity"=>"Luleaa", 
-      "AddressCountry"=>"Sweden", 
-      "AddressStreet"=>{"xsi:nil"=>"true"}, 
-      "FirstName"=>"Oskar", 
-      "Gender"=>"Male", 
-      "LastName"=>"Gewalli", 
-      "PictureUri"=>{"xsi:nil"=>"true"}
+      "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance",
+      "xmlns:xsd"=>"http://www.w3.org/2001/XMLSchema",
+      "xmlns"=>"http://schemas.datacontract.org/2004/07/Customers",
+      "AccountNumber"=>"1",
+      "AddressCity"=>"Malmö",
+      "AddressCountry"=>"Sweden",
+      "AddressStreet"=>{ "xsi:nil"=>"true" },
+      "FirstName"=>"Oskar",
+      "Gender"=>"Male",
+      "LastName"=>"Gewalli",
+      "PictureUri"=>{ "xsi:nil"=>"true" }
     }
-    post "/CustomerService.svc/SaveCustomer", params: { :Customer => customer, #$single_customer
-      'CONTENT_TYPE' => 'application/xml', 
+    post "/CustomerService.svc/SaveCustomer", params: { :Customer => customer, # $single_customer
+      "CONTENT_TYPE" => "application/xml",
       :format => "xml"
     }
     assert_response :success
     oskar = Customer.find_by(account_number: 1)
-    assert_equal( 'Luleaa', oskar.address_city )
-    assert_equal( 'Sweden', oskar.address_country )
-    assert_equal( nil, oskar.address_street )
+    assert_equal("Malmö", oskar.address_city)
+    assert_equal("Sweden", oskar.address_country)
+    assert_equal("Lugna gatan 40", oskar.address_street)
   end
 end
